336b48aacf51fe632ebb03fafc7d5054f0434600
hunt9968 <m.komaya.2004@gmail.com>
2024-09-04 19:27:46 +0900

    react+tsへの移行をある程度進めた mainプロセスの処理やpreloadの書き方が未完成

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,4 +1,4 @@
-import { contextBridge } from 'electron'
+import { ipcRenderer, contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
@@ -11,6 +11,21 @@ if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
     contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld('myAPI', {
+      openDialog: () => ipcRenderer.invoke('open-dialog'),
+      openMultipleDialog: () => ipcRenderer.invoke('open-multiple-dialog'),
+      openOutputDialog: () => ipcRenderer.invoke('open-output-dialog'),
+      //同期処理
+      selectCodec: (codec: string) => ipcRenderer.send('select-codec', codec),
+      selectSuffix: (suffix: string) => ipcRenderer.send('select-suffix', suffix),
+      startFfmpeg: () => ipcRenderer.send('start-ffmpeg'),
+      checkFilePath: (callback) => ipcRenderer.on('check-file-path', (_event, message: string) => {
+          callback(message);
+      }),
+      ffmpegLog: (callback) => ipcRenderer.on('ffmpeg-log', (_event, message: string) => {
+          callback(message);
+      }),
+  }); 
   } catch (error) {
     console.error(error)
   }
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,6 +3,8 @@ import electronLogo from './assets/electron.svg'
 
 function App(): JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const button = (): void => window.myAPI.openDialog();
+  const multiImportButton = (): void => window.myAPI.mpenMultipleDialog();
 
   return (
     <>
@@ -16,6 +18,47 @@ function App(): JSX.Element {
         Please try pressing <code>F12</code> to open the devTool
       </p>
       <div className="actions">
+        <div className= "action" id="button">
+          <a onClick={button}>Select file</a>
+        </div>
+        <p id="text" />
+        <div className= "action" id="multiImportButton"><a>Select multiple files</a></div>
+        <p id="inputList" />
+        <select id="codecSelect">
+          <option value="">--Select codec--</option>
+          <option value="h264_nvenc">NVIDIA NVENC H.264</option>
+          <option value="hevc_nvenc">NVIDIA NVENC H.265</option>
+          <option value="av1_nvenc">NVIDIA NVENC av1</option>
+          <option value="libx264">H.264</option>
+          <option value="libx265">H.265</option>
+          <option value="ilbvpx-vp9">libvpx VP9</option>
+          <option value="libaom-av1">libaom AV1</option>
+          <option value="dnxhd">DNxHR</option>
+          <option value="cfhd">GoPro CineForm HD</option>
+          <option value="prores_ks">Apple ProRes(iCodec Pro)</option>
+        </select>
+        <br />
+        <select id="suffixSelect">
+          <option value="">(none)</option>
+          <option value="_codec">_codec</option>
+          <option value="_bitrate">_bitrate</option>
+          <option value="_converted">_converted</option>
+          <option value="_FFmpegGUI">_FFmpegGUI</option>
+        </select>
+        <br />
+        <div className= "action" id="outputButton">
+          <a>Select output folder</a>
+        </div>
+        <p id="outputFolder" />
+      </div>
+      <div className='actions'>
+        <div className= "action" id="encodeButton">
+          <a>Encode</a>
+        </div>
+        <p id="checkFilePath" />
+        <p id="ffmpegLog" />
+      </div>
+      <div className='actions'>
         <div className="action">
           <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
             Documentation
@@ -27,6 +70,7 @@ function App(): JSX.Element {
           </a>
         </div>
       </div>
+
       <Versions></Versions>
     </>
   )
