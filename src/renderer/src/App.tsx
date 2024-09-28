35bea2d09ec37c0049ee96fa893c3fc39d0d7147
hunt9968 <m.komaya.2004@gmail.com>
2024-09-28 19:38:49 +0900

    CodecSelectをコンポーネントとして切り分け

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ImportButton from './components/ImportButton'
 import MultiImportButton from './components/MultiImportButton'
+import CodecSelect from './components/CodecSelect'
 
 function App(): JSX.Element {
   const [inputFilePath, setInputFile] = React.useState<string>('')
   const [inputFileList, setInputList] = React.useState<string[]>([])
+  const [selectedCodec, setCodec] = React.useState<string>('')
   const inputListMap = inputFileList.map((filePath) => <li key={filePath}>{filePath}</li>)
 
   return (
@@ -14,19 +16,7 @@ function App(): JSX.Element {
         <div className="text">{`Input: ${inputFilePath}`}</div>
         <MultiImportButton setInputList={setInputList} />
         <div className="text">{inputListMap}</div>
-        <select id="codecSelect">
-          <option value="">--Select codec--</option>
-          <option value="h264_nvenc">NVIDIA NVENC H.264</option>
-          <option value="hevc_nvenc">NVIDIA NVENC H.265</option>
-          <option value="av1_nvenc">NVIDIA NVENC av1</option>
-          <option value="libx264">H.264</option>
-          <option value="libx265">H.265</option>
-          <option value="ilbvpx-vp9">libvpx VP9</option>
-          <option value="libaom-av1">libaom AV1</option>
-          <option value="dnxhd">DNxHR</option>
-          <option value="cfhd">GoPro CineForm HD</option>
-          <option value="prores_ks">Apple ProRes(iCodec Pro)</option>
-        </select>
+        <CodecSelect setCodec={setCodec} selectedCodec={''} />
         <br />
         <select id="suffixSelect">
           <option value="">(none)</option>
---
+++ src/renderer/src/components/CodecSelect.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+type CodecSelectProps = {
+    setCodec: React.Dispatch<React.SetStateAction<string>>
+    selectedCodec: string
+}
+
+const CodecSelect: React.FC<CodecSelectProps> = ({ selectedCodec,setCodec }) => {
+    const handleCodec = (event: SelectChangeEvent) => {
+        setCodec(event.target.value as string);
+    };
+
+    return (
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="select-codec-label">Codec</InputLabel>
+                <Select 
+                    labelId='select-codec-label'
+                    id="codecSelect"
+                    label="Codec"
+                    value={selectedCodec}
+                    onChange={handleCodec}
+
+                    >
+                    <MenuItem value="h264_nvenc">NVIDIA NVENC H.264</MenuItem>
+                    <MenuItem value="hevc_nvenc">NVIDIA NVENC H.265</MenuItem>
+                    <MenuItem value="av1_nvenc">NVIDIA NVENC av1</MenuItem>
+                    <MenuItem value="libx264">H.264</MenuItem>
+                    <MenuItem value="libx265">H.265</MenuItem>
+                    <MenuItem value="ilbvpx-vp9">libvpx VP9</MenuItem>
+                    <MenuItem value="libsvtav1">SVT-AV1</MenuItem>
+                    <MenuItem value="dnxhd">DNxHR</MenuItem>
+                    <MenuItem value="cfhd">GoPro CineForm HD</MenuItem>
+                    <MenuItem value="prores_ks">Apple ProRes(iCodec Pro)</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+    )
+}
+
+export default CodecSelect
