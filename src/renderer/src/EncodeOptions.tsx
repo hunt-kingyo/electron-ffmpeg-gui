aef95dd38869c5fc65990e3c898cb7358ce127ea
hunt9968 <m.komaya.2004@gmail.com>
2024-11-27 13:55:50 +0900

    EncodeOptionsの型定義を追加

--- src/renderer/src/EncodeOptions.tsx
+++ src/renderer/src/EncodeOptions.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const [selectedCodec, setCodec] = React.useState<string>('')
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -7,7 +7,14 @@ const [selectedFormat, setFormat] = React.useState<string>('')
 const [outputFolder, setOutputFolder] = React.useState<string>('')
 
 interface EncodeOptions {
-    
+    videoCodec:string
+    option:string
+    format:string
+    pixelFormat:string
+    videoBitrate:string
+    outputFolder:string
 }
 
-export const useEncodeOptions = ()
+export const useEncodeOptions = (
+    
+)
