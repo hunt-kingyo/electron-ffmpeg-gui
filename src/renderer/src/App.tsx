447b374a7ed7f5e3f6234be7aca69db9aae878b6
hunt9968 <m.komaya.2004@gmail.com>
2024-12-10 15:00:57 +0900

    materialUIのgrid, box, stackを追加

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,6 +7,7 @@ import SelectContainerFormat from './components/SelectContainerFormat'
 import EncodeButton from './components/EncodeButton'
 import OutputButton from './components/OutputButton'
 import { useEncodeOptions } from './components/useEncodeOptions'
+import { Box, Grid, Grid2, Stack } from '@mui/material'
 
 function App(): JSX.Element {
 
@@ -29,32 +30,43 @@ function App(): JSX.Element {
   return (
     <div>
       <div className="actions">
-        <ImportButton setInputFile={setInputFile} />
-        <div className="text">{`Input: ${inputFilePath}`}</div>
-        <MultiImportButton setInputList={setInputList} />
-        <div className="text">{inputListMap}</div>
-        <SelectVideoCodec onVideoCodecChange={setVideoCodec} videoCodec={encodeOptions.videoCodec} />
-        {encodeOptions.videoCodec}
-        <br />
-        <select id="suffixSelect">
-          <option value="">(none)</option>
-          <option value="_codec">_codec</option>
-          <option value="_bitrate">_bitrate</option>
-          <option value="_converted">_converted</option>
-          <option value="_FFmpegGUI">_FFmpegGUI</option>
-        </select>
-        <SelectCodecOption onCodecOptionChange={setCodecOption} videoCodec={encodeOptions.videoCodec} codecOption={encodeOptions.codecOption}/>
-        {encodeOptions.codecOption[0]}
-        <br />
-        <SelectContainerFormat onContainerFormatChange={setContainerFormat} videoCodec={encodeOptions.videoCodec} containerFormat={encodeOptions.containerFormat} />
-        {encodeOptions.containerFormat}
-        <OutputButton onOutputFolderChange={setOutputFolder} />
-        <div className='text'>{encodeOptions.outputFolder}</div>
+        <Grid2 container spacing={2}>
+          <Grid2 size={8}>
+            <Stack spacing={0.5}>
+              <ImportButton  setInputFile={setInputFile} />
+              <div className="text">{`Input: ${inputFilePath}`}</div>
+              <MultiImportButton setInputList={setInputList} />
+              <div className="text">{inputListMap}</div>
+              <Box sx={{height:400, bgcolor:'#4c4c4c'}}>filepath list</Box>
+            </Stack>
+          </Grid2>
+          <Grid2 size={4}>
+            <Stack spacing={0.5}>
+            <SelectVideoCodec onVideoCodecChange={setVideoCodec} videoCodec={encodeOptions.videoCodec} />
+            {encodeOptions.videoCodec}
+            <br />
+            <select id="suffixSelect">
+              <option value="">(none)</option>
+              <option value="_codec">_codec</option>
+              <option value="_bitrate">_bitrate</option>
+              <option value="_converted">_converted</option>
+              <option value="_FFmpegGUI">_FFmpegGUI</option>
+            </select>
+            <SelectCodecOption onCodecOptionChange={setCodecOption} videoCodec={encodeOptions.videoCodec} codecOption={encodeOptions.codecOption}/>
+            {encodeOptions.codecOption[0]}
+            <br />
+            <SelectContainerFormat onContainerFormatChange={setContainerFormat} videoCodec={encodeOptions.videoCodec} />
+            {encodeOptions.containerFormat}
+            <br />
+            <OutputButton onOutputFolderChange={setOutputFolder} />
+            <div className='text'>{encodeOptions.outputFolder}</div>
+            <EncodeButton />
+            <p id="checkFilePath" />
+            <p id="ffmpegLog" />
+            </Stack>
+          </Grid2>
+        </Grid2>
       </div>
-      <EncodeButton />
-      <p id="checkFilePath" />
-      <p id="ffmpegLog" />
-
     </div>
   )
 }
